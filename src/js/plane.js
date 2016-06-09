c1d558568b542da8c37e7e5c8297325b7cbee623
migdall <jesus.noland@litpen.com>
2016-06-09 12:38:07 -0700

    Adjusting the shape of the cockpit

--- src/js/plane.js
+++ src/js/plane.js
@@ -276,8 +276,25 @@ var AirPlane = function() {
     this.mesh = new THREE.Object3D();
     
     // Create the cabin
+    // Cockpit
+
     var geomCockpit = new THREE.BoxGeometry(60, 50, 50, 1, 1, 1);
     var matCockpit = new THREE.MeshPhongMaterial({color: Colors.red, shading: THREE.FlatShading});
+    
+    // We can access a specific vertex of a shape through
+    // the vertices array, and then move its x, y, and z property:
+    geomCockpit.vertices[4].y -= 10;
+    geomCockpit.vertices[4].z += 20;
+
+    geomCockpit.vertices[5].y -= 10;
+    geomCockpit.vertices[5].z -= 20;
+    
+    geomCockpit.vertices[6].y += 30;
+    geomCockpit.vertices[6].z += 20;
+    
+    geomCockpit.vertices[7].y += 30;
+    geomCockpit.vertices[7].z -= 20;
+    
     var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
     cockpit.castShadow = true;
     cockpit.receiveShadow = true;
